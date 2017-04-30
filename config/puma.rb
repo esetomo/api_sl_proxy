430b8802c5603708229bcaec070cf1063953e4db
Ichigo Mayo <esetomo@gmail.com>
2017-04-30 23:23:10 +0900

    added puma state path

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 _proj_path = File.expand_path("../..", __FILE__)
 
 pidfile "#{_proj_path}/tmp/pids/puma.pid"
+state_path "#{_proj_path}/tmp/pids/puma.state"
 bind "unix://#{_proj_path}/tmp/sockets/puma.sock"
 directory _proj_path
 
